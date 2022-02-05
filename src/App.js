a05bd5b720ee6c5cd2f35b92939953356288d999
amarealcoder <ugorjimiracle06@gmail.com>
2022-02-05 16:56:38 +0100

    Files: Push more base files to the repo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from './logo.svg';
+// import './App.css';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import Authentication from './pages/Auth';
+import Home from './pages/Home';
 
 function App() {
+  // const classes = useStyles();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route path='/authentication'>
+        <Authentication />
+      </Route>
+      <Route path='/home'>
+        <Home />
+      </Route>
+    </Switch>
   );
 }
 
