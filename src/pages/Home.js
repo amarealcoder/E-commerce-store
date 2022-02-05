9e33d2f4479d21744ad53ffc26c67cd1d628694d
amarealcoder <ugorjimiracle06@gmail.com>
2022-02-05 16:47:21 +0100

    Style: Add more styles on the home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import styles from './Home.module.css';
 
-import menuIcon from '../images/menu-variant.svg';
-import logoIcon from '../images/Logo.svg';
-import avatarIcon from '../images/Avatar.svg';
+import menuIcon from '../images/menu-variant.png';
+import logoIcon from '../images/Logo.png';
+import avatarIcon from '../images/Avatar.png';
+import headSet from '../images/headset.png';
+import rightArrow from '../images/arrow-right.png';
 
 const Home = () => {
   return (
@@ -41,6 +43,67 @@ const Home = () => {
           <li>headband</li>
           <li>earpods</li>
         </ul>
+
+        <div className={styles.prodContainer}>
+          <div className={styles.prodList}>
+            <div className={styles.prodDesc}>
+              <h2>TMA-2 Modular Headphone</h2>
+              <div className={styles.shopNow}>
+                <a href=''>
+                  Shop now
+                  <span>
+                    <img src={rightArrow} />
+                  </span>
+                </a>
+              </div>
+            </div>
+            <div className={styles.prodImage}>
+              <img src={headSet} alt='a black headphone' />
+            </div>
+          </div>
+
+          <div className={styles.prodList}>
+            <div className={styles.prodDesc}>
+              <h2>TMA-2 Modular Headphone</h2>
+              <div className={styles.shopNow}>
+                <a href=''>
+                  Shop now
+                  <span>
+                    <img src={rightArrow} />
+                  </span>
+                </a>
+              </div>
+            </div>
+            <div className={styles.prodImage}>
+              <img src={headSet} alt='a black headphone' />
+            </div>
+          </div>
+        </div>
+
+        <div className={styles.featured}>
+          <p>Featured Products</p>
+          <a href=''>See all</a>
+        </div>
+
+        <div className={styles.featuredContainer}>
+          <div className={styles.featuredProducts}>
+            <img src={headSet} alt='a product image' />
+            <p>TMA-2 HD Wirelss</p>
+            <span>USD 350</span>
+          </div>
+
+          <div className={styles.featuredProducts}>
+            <img src={headSet} alt='a product image' />
+            <p>TMA-2 HD Wirelss</p>
+            <span>USD 350</span>
+          </div>
+
+          <div className={styles.featuredProducts}>
+            <img src={headSet} alt='a product image' />
+            <p>TMA-2 HD Wirelss</p>
+            <span>USD 350</span>
+          </div>
+        </div>
       </section>
     </div>
   );
