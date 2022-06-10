469bb4c9e68d9357f3a33da319ebe244a0cd96d8
amarealcoder <ugorjimiracle06@gmail.com>
2022-06-10 19:24:27 +0100

    Feature: Add to cart

--- src/components/ui/ShoppingCart.js
+++ src/components/ui/ShoppingCart.js
@@ -1,61 +1,64 @@
 import styles from './ShoppingCart.module.css';
+import ReactDom from 'react-dom';
 
-import chevronRight from '../images/chevron-left.png';
-import trash from '../images/trash.png';
-import headset from '../images/headset.png';
+import { FaTrash } from 'react-icons/fa';
+
 import Button from './Button';
+import ProductsHeader from '../products/ProductsHeader';
+import React from 'react';
 
-const ProductsHeader = () => {
-  return (
+const CartOverlay = ({setIsOpen}) => {
+  return <div onClick={() => setIsOpen(false)} className={styles.cartOverlay}></div>
+}
+
+const Cart = ({setIsOpen, cartItems}) => {
+  
+   return (
     <div className={styles.container}>
-      <header className={styles.headerContainer}>
-        <img src={chevronRight} alt='a right angle' />
+      <ProductsHeader> 
+        <h2 onClick={() => setIsOpen(false)}> x</h2>
         <h4>Shopping Cart</h4>
-        <img src={trash} />
-      </header>
+        <FaTrash style={{cursor: 'pointer'}}/>
+      </ProductsHeader>
       <section className={styles.cartItems}>
-        <div className={styles.content}>
+        {cartItems?.map(product => <div key={product.id} className={styles.content}>
           <div className={styles.imageContainer}>
-            <img src={headset} alt='' />
+            <img src={product.image} alt={product.title} />
           </div>
           <div className={styles.imageInfo}>
-            <p>TMA-2 Comfort Wireless</p>
-            <p className={styles.amount}>USD 305</p>
+            <p>{product.title}</p>
+            <p className={styles.amount}>{product.price}</p>
             <div className={styles.actionGroup}>
               <div className={styles.buttons}>
                 <button>-</button>
                 <span>1</span>
                 <button>+</button>
               </div>
-              <img src={trash} alt='' />
+              <FaTrash />
             </div>
           </div>
         </div>
-
-        <div className={styles.content}>
-          <div className={styles.imageContainer}>
-            <img src={headset} alt='' />
-          </div>
-          <div className={styles.imageInfo}>
-            <p>TMA-2 Comfort Wireless</p>
-            <p className={styles.amount}>USD 305</p>
-            <div className={styles.actionGroup}>
-              <div className={styles.buttons}>
-                <button>-</button>
-                <span>1</span>
-                <button>+</button>
-              </div>
-              <img src={trash} alt='' />
-            </div>
-          </div>
-        </div>
+        )}
       </section>
       <div className={styles.total}>
         <p className={styles.totalItems}>Total 1 Item</p>
         <p className={styles.totalAmount}>USD 305</p>
       </div>
-      <Button>Proceed to Checkout </Button>
+      <div className={styles.buttonContainer}>
+        <Button>Proceed to Checkout </Button>
+      </div>
     </div>
   );
 };
-export default ProductsHeader;
+
+
+const ShoppingCart = ({cartItems, setIsOpen}) => {
+  return <React.Fragment>
+    {ReactDom.createPortal(<CartOverlay setIsOpen={setIsOpen}/>, document.getElementById('cartOverlay-root'))}
+    {ReactDom.createPortal(<Cart cartItems={cartItems} setIsOpen={setIsOpen} />, document.getElementById('cart-root'))}
+    </React.Fragment>
+  
+}
+export default ShoppingCart;
+
+ 
