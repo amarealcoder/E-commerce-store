5a9de4f76b2578c174b2b646a5324df6d81e3dcd
amarealcoder <ugorjimiracle06@gmail.com>
2022-06-27 08:48:14 +0100

    Fix home page error message

--- src/services/productsApi.js
+++ src/services/productsApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const productsApi = createApi({
   reducerPath: 'products',
-  baseQuery: fetchBaseQuery({ baseUrl: 'https://fakestoreapi.com' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://fakestoreap.com' }),
   endpoints: (builder) => ({
     getProducts: builder.query({
       query: () => `products`,
