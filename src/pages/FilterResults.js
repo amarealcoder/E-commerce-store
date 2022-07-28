107f0225e836fef0b9bca1d17b230a77a01e14e5
amarealcoder <ugorjimiracle06@gmail.com>
2022-07-28 15:04:17 +0100

    fix: Filter results page

--- src/pages/FilterResults.js
+++ src/pages/FilterResults.js
@@ -13,7 +13,7 @@ import FilterModal from './FilterModal';
 import { FaSpinner, FaChevronLeft } from 'react-icons/fa';
 
 const SearchResults = () => {
-  const history = useHistory(); 
+  const history = useHistory();
   const [isOpen, setIsOpen] = useState(false);
   const { data, isSuccess, isLoading } = useGetProductsQuery();
   const [filtered, setFiltered] = useState([]);
@@ -22,38 +22,77 @@ const SearchResults = () => {
   const [pageCategory, setPageCategory] = useState('');
   const [isFiltered, setIsFiltered] = useState(false);
 
-
-  const handleFilter = useCallback((category, title) => {
-    const filteredCategories = isSuccess && data?.filter(item => item.category === category);
-    setPageTitle(title);
-    setPageCategory(category);
-    setFiltered(filteredCategories);
-  }, [isSuccess, data]); 
+  const handleFilter = useCallback(
+    (category, title) => {
+      const filteredCategories =
+        isSuccess && data?.filter((item) => item.category === category);
+      setPageTitle(title);
+      setPageCategory(category);
+      setFiltered(filteredCategories);
+    },
+    [isSuccess, data]
+  );
 
   const renderProducts = () => {
-    if(!isFiltered){
-    return (isSuccess && data?.map(item => <Product key={item.id} image={item.image} title={item.title} price={item.price}>
-      <div className={styles.rating}>
-        <Rating rating={item.rating.rate} count={item.rating.count}/>
-      </div>
-    </Product>))
-    }else{
-    return (isSuccess && data && filtered?.map(item => <Product key={item.id} image={item.image} title={item.title} price={item.price}>
-      <div className={styles.rating}>
-        <Rating rating={item.rating.rate} count={item.rating.count}/>
-      </div>
-    </Product>))
+    if (!isFiltered) {
+      return (
+        isSuccess &&
+        data?.map((item) => (
+          <Product
+            key={item.id}
+            image={item.image}
+            title={item.title}
+            price={item.price}
+            onClick={() => history.push(`/details/${item.id}`)}
+          >
+            <div className={styles.rating}>
+              <Rating rating={item.rating.rate} count={item.rating.count} />
+            </div>
+          </Product>
+        ))
+      );
+    } else {
+      return (
+        isSuccess &&
+        data &&
+        filtered?.map((item) => (
+          <Product
+            key={item.id}
+            image={item.image}
+            title={item.title}
+            price={item.price}
+            onClick={() => history.push(`/details/${item.id}`)}
+          >
+            <div className={styles.rating}>
+              <Rating rating={item.rating.rate} count={item.rating.count} />
+            </div>
+          </Product>
+        ))
+      );
     }
-  }
-
+  };
 
   return (
     <div>
-      {isOpen && <FilterModal  setIsOpen={setIsOpen} setFiltered={setFiltered} setIsFiltered={setIsFiltered}/>}
+      {isOpen && (
+        <FilterModal
+          setIsOpen={setIsOpen}
+          setFiltered={setFiltered}
+          setIsFiltered={setIsFiltered}
+        />
+      )}
       <section className={styles.container1}>
-        <ProductsHeader >
-          <FaChevronLeft onClick={() => history.push('/')}/>
-          <button style={{marginLeft: '5%',  marginBottom: '5px', cursor: 'pointer'}} onClick={() => setIsFiltered(false)}>See all</button>
+        <ProductsHeader>
+          <FaChevronLeft
+            style={{ cursor: 'pointer' }}
+            onClick={() => history.goBack()}
+          />
+          <button
+            style={{ marginLeft: '5%', marginBottom: '5px', cursor: 'pointer' }}
+            onClick={() => setIsFiltered(false)}
+          >
+            See all
+          </button>
         </ProductsHeader>
         <div className={styles.title}>
           <p>{isSuccess && pageCategory}</p>
@@ -65,32 +104,36 @@ const SearchResults = () => {
             Filter
           </button>
           <ul>
-            {isSuccess && filteredCategory.map(item => (
-            <li key={item.id} 
-              onClick={() => {
-                setIsFiltered(true);
-                handleFilter(item.category, item.title)}}
+            {isSuccess &&
+              filteredCategory.map((item) => (
+                <li
+                  key={item.id}
+                  onClick={() => {
+                    setIsFiltered(true);
+                    handleFilter(item.category, item.title);
+                  }}
                 >
                   {item.category}
-            </li>))}
+                </li>
+              ))}
           </ul>
         </div>
       </section>
 
       <section className={styles.container2}>
         {isLoading && (
-            <span>
-              <FaSpinner
-                style={{
-                  fontSize: '50px',
-                  position: 'absolute',
-                  left: '50%'
-                }}
-                className={styles.loadingIcon}
-              />
-            </span>
+          <span>
+            <FaSpinner
+              style={{
+                fontSize: '50px',
+                position: 'absolute',
+                left: '50%',
+              }}
+              className={styles.loadingIcon}
+            />
+          </span>
         )}
-        {renderProducts()} 
+        {renderProducts()}
       </section>
     </div>
   );
